05c84c0be4b5d7cfd2133ac8b8e1a810792bfbf1
dvisign <jmch0922@gmail.com>
2024-03-12 14:31:50 +0900

    롤업 빌드에러 수정

--- rollup.config.mjs
+++ rollup.config.mjs
@@ -5,9 +5,9 @@ import json from "@rollup/plugin-json";
 import { terser } from "rollup-plugin-terser";
 
 export default {
-  input: "dist/index.ts", // 가정된 시작점
+  input: "src/index.ts", // 가정된 시작점
   output: {
-    file: "dist/index.js",
+    dir: "dist",
     format: "cjs",
     sourcemap: process.env.NODE_ENV === "production",
   },
